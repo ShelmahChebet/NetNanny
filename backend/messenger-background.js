40bab16a9894d4e6220a4af05440ad37b838d6e7
laygofiona <laygo.fiona@gmail.com>
2025-02-15 14:05:10 -0500

    Deleting files for messenger and instagram

--- backend/messenger-background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "analyzeMessages") {
-        console.log("Messages Extracted:", request.messages);
-        return true;  // Keeps the message channel open for async response
-    }
-    return false;
-});
